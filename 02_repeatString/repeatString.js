d4c4a0dedf6fc0fd10a372cf7256e131be127eb2
shakey200592 <shakey1992jm@hotmail.co.uk>
2024-05-28 00:34:21 +0100

    feat: complete excercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string,numRepeat) 
+{
+    if (numRepeat < 0)
+    {
+        return "ERROR";
+    }
+    let newString = ''
+    for(let i =0; i <numRepeat; i++)
+    {
+        newString += string
+    }
 
+    return newString;
 };
 
 // Do not edit below this line
