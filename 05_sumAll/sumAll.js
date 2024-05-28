83f93e7c00c074339a61ef9b81405c7018d76d3e
shakey200592 <shakey1992jm@hotmail.co.uk>
2024-05-28 02:33:40 +0100

    feat: complete excercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(smallestNumber, largestNumber) {
+
+    if (smallestNumber < 0 || largestNumber < 0 || 
+        !Number.isInteger(smallestNumber) || !Number.isInteger(largestNumber)) return "ERROR";
+    
+    if (smallestNumber > largestNumber) {
+        [smallestNumber,largestNumber] = [largestNumber,smallestNumber]
+    }
+
+    let sum = 0;
+
+    for(let i = smallestNumber; i <= largestNumber; ++i) {
+        sum += i;
+    }
+    return sum;
 
 };
 
