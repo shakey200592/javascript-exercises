b33153db2e902472501c9918f3792cc718c9a433
shakey200592 <shakey1992jm@hotmail.co.uk>
2024-05-28 02:01:53 +0100

    feat: complete excercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) 
+{ 
+    return arr.filter(val => !valuesToRemove.includes(val));
+
 };
 
 // Do not edit below this line
