cc62efb3923e9ab006dcdf79fe71038596de93d6
Jake McDermott <shakey1992jm@hotmail.co.uk>
2024-09-16 00:37:19 +0100

    feat: complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const validChars = "abcdefghijklmnopqrstuvwxyz123456789";
+  const cleanedString = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => validChars.includes(character))
+    .join("");
 
+  const reversedString = cleanedString.split("").reverse().join("");
+
+  return cleanedString === reversedString ? true : false;
 };
 
 // Do not edit below this line
