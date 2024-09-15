b3907db9156a233463c5893cb9d6752f59fd622d
Jake McDermott <shakey1992jm@hotmail.co.uk>
2024-09-16 00:00:05 +0100

    feat: complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,31 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, val) => acc + val, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, val) => acc * val, 1);
 };
 
-const power = function() {
-	
+const power = function (num, power) {
+  return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function (fac) {
+  const factorialArray = [];
+  for (let i = 1; i <= fac; i++) {
+    factorialArray.push(i);
+  }
+  return factorialArray.reduce((acc, val) => acc * val, 1);
 };
+factorial(2);
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
