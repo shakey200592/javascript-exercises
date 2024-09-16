75535d7e3ba0a6fb142882fcbd27e5288b1d1a23
Jake McDermott <shakey1992jm@hotmail.co.uk>
2024-09-16 01:25:16 +0100

    feat: complete 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  let titles = [];
+  books.forEach((element) => {
+    titles.push(element.title);
+  });
+  return titles;
 };
+
+const book = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
+
+console.log(getTheTitles(book));
 
 // Do not edit below this line
 module.exports = getTheTitles;
