10a9383a29d26682123fcb22d84350ab9dc188d8
Jake McDermott <shakey1992jm@hotmail.co.uk>
2024-09-16 01:15:55 +0100

    feat: complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (count) {
+  if (typeof count === "string") {
+    count = parseInt(count);
+  }
+  if (count === 0) {
+    return 0;
+  } else if (count < 0) {
+    return `OOPS`;
+  }
 
+  let fib = [0, 1];
+
+  for (let i = 0; i <= count - 2; i++) {
+    fib.push(fib[i] + fib[i + 1]);
+  }
+  return fib[fib.length - 1];
 };
+
+console.log(fibonacci("0"));
 
 // Do not edit below this line
 module.exports = fibonacci;
